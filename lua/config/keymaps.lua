8f80004adc4bbaff478869c554254ea86e720e47
Camilo Avelar <camilo.avelar@sejaefi.com.br>
2023-11-30 17:20:21 -0300

    feat: melhorias e novos plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,9 +8,10 @@ mapkey("<leader>`", "e #", "n") -- Switch to Other Buffer
 
 -- Directory Navigatio}n
 mapkey("<leader>m", "NvimTreeFocus", "n")
+mapkey("<leader>pv", "NvimTreeToggle", "n")
 mapkey("<leader>e", "NvimTreeToggle", "n")
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")

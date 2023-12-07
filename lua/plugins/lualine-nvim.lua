dbfd888ff5c82105a96284bdb84c99d0cd3d2889
CamiloAvelar <camilo.avelar@gmail.com>
2023-12-07 09:13:17 -0300

    refactor: change commit message to statusline

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -1,3 +1,6 @@
+vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+local git_blame = require('gitblame')
+
 local config = function()
 	require("lualine").setup({
 		options = {
@@ -9,6 +12,9 @@ local config = function()
 		sections = {
 			lualine_a = { "mode" },
 			lualine_b = { "buffers" },
+      lualine_c = {
+        { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
+      },
 			lualine_x = { "encoding", "fileformat", "filetype" },
 			lualine_y = { "progress" },
 			lualine_z = { "location" },

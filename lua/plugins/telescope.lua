9eeaed1f34a4cf7adb87b49858da8e9f868c1fc4
CamiloAvelar <camilo.avelar@gmail.com>
2023-12-10 17:23:50 -0300

    feat(telescope): add keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -38,14 +38,21 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.3",
 	lazy = false,
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-symbols.nvim",
+  },
 	config = config,
 	keys = {
 		mapkey("<leader>fk", "Telescope keymaps", "n"),
 		mapkey("<leader>fh", "Telescope help_tags", "n"),
 		mapkey("<leader>pf", "Telescope find_files", "n"),
 		mapkey("<C-p>", "Telescope git_files", "n"),
+    mapkey("<leader>fd", "Telescope diagnostics", "n"),
 		mapkey("<leader>fg", "Telescope live_grep", "n"),
 		mapkey("<leader>fb", "Telescope buffers", "n"),
+		mapkey("<leader>fs", "Telescope symbols", "n"),
+		mapkey("<leader>gc", "Telescope git_commits", "n"),
+		mapkey("<leader>gb", "Telescope git_branches", "n"),
 	},
 }

fe3a8b2d567c60ed7d3b3dc7159999a6162bfe7a
CamiloAvelar <camilo.avelar@gmail.com>
2023-12-10 20:41:18 -0300

    feat(theme): add background

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,12 @@
 return {
 	"catppuccin/nvim",
 	lazy = false,
-	priority = 999,
 	config = function()
-		vim.cmd("colorscheme catppuccin")
-		require("catppuccin").setup({})
+		require("catppuccin").setup({
+      	transparent_background = true,
+      	term_colors = true,
+    })
+
+    vim.cmd("colorscheme catppuccin")
 	end,
 }

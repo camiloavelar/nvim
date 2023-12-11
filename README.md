7a172e911d042a378b70b72c54d5e96ebe51cd7e
CamiloAvelar <camilo.avelar@gmail.com>
2023-12-10 22:29:28 -0300

    feat(plugins): rename files and add more plugins

--- README.md
+++ README.md
@@ -6,5 +6,3 @@ Prerequisite:
 - install [ripgrep](https://github.com/BurntSushi/ripgrep).
 - install [nvm](https://github.com/nvm-sh/nvm).
 - install [go](https://go.dev/doc/install).
-
-
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,26 @@
 vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
 local git_blame = require('gitblame')
 
+local lspStatus = {
+  function()
+    local msg = "No LSP detected"
+    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+      return msg
+    end
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+        return client.name
+      end
+    end
+    return msg
+  end,
+  icon = "",
+  color = { fg = "#d3d3d3" },
+}
+
 local config = function()
 	require("lualine").setup({
 		options = {
@@ -18,7 +38,7 @@ local config = function()
       lualine_c = {
         { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
       },
-			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_x = { "encoding", "fileformat", "filetype", lspStatus },
 			lualine_y = { "progress" },
 			lualine_z = { "location" },
 		},

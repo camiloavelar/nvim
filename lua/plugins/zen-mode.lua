2ca6b63ce7919c49c8cee2a161445ba09bb477c9
CamiloAvelar <camilo.avelar@gmail.com>
2023-12-12 10:44:27 -0300

    refactor(zenmode): set correct window size

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -2,8 +2,8 @@ return {
   "folke/zen-mode.nvim",
   lazy = false,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+    window = {
+      width = .7
+    }
+  },
 }

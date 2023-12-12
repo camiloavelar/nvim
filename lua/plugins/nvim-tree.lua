e5f801ffd38da1f223b791d88567d4fd3b51ed0b
CamiloAvelar <camilo.avelar@gmail.com>
2023-12-12 09:43:22 -0300

    refactor(tree): set to floating and back to NetRW

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,13 +1,45 @@
+local width_ratio = 0.5
+local height_ratio = 0.8
+
 return {
 	"nvim-tree/nvim-tree.lua",
 	lazy = false,
 	config = function()
 		require("nvim-tree").setup({
+      hijack_netrw = false,
 			filters = {
 				dotfiles = false,
 			},
+      actions = {
+        open_file = {
+          quit_on_open = true
+        }
+      },
 			view = {
-				adaptive_size = true,
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+
+            local window_w = screen_w * width_ratio
+            local window_h = screen_h * height_ratio
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+              border = "rounded",
+              relative = "editor",
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+				adaptive_size = false,
+        relativenumber = true,
 			},
 		})
 	end,

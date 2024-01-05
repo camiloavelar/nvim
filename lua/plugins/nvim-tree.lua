edef9046bba797bb5edf0109d2d859a42d70d57b
Camilo Avelar <camilo.avelar@gerencianet.com.br>
2024-01-05 13:28:55 +0000

    feat(tree): add close function with <C-c>

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,26 @@
 local width_ratio = 0.5
 local height_ratio = 0.8
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-c>', api.tree.close,        opts('Close'))
+  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
 
 return {
 	"nvim-tree/nvim-tree.lua",
 	lazy = false,
 	config = function()
 		require("nvim-tree").setup({
+      on_attach = my_on_attach,
       hijack_netrw = false,
 			filters = {
 				dotfiles = false,

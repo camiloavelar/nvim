6441bb0e0388698b8525f5e63b35bd9812d74a43
Camilo Avelar <camilo.avelar@gerencianet.com.br>
2024-01-05 23:34:42 +0000

    refactor(trouble): change keymaps

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -8,12 +8,28 @@ return {
     -- refer to the configuration section below
   },
   config = function ()
-    vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end, { desc = "Trouble" })
-    vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end, { desc = "Trouble: Workspace Diagnostics" })
-    vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end, { desc = "Trouble: Document Diagnostics" })
-    vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end, { desc = "Trouble: Quickfix" })
-    vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end, { desc = "Trouble: Loclist" })
+    vim.keymap.set("n", "<leader>tt", function() require("trouble").toggle() end, { desc = "Trouble" })
+    vim.keymap.set("n", "<leader>tw", function() require("trouble").toggle("workspace_diagnostics") end, { desc = "Trouble: Workspace Diagnostics" })
+    vim.keymap.set("n", "<leader>td", function() require("trouble").toggle("document_diagnostics") end, { desc = "Trouble: Document Diagnostics" })
+    vim.keymap.set("n", "<leader>tq", function() require("trouble").toggle("quickfix") end, { desc = "Trouble: Quickfix" })
+    vim.keymap.set("n", "<leader>tl", function() require("trouble").toggle("loclist") end, { desc = "Trouble: Loclist" })
     vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end, { desc = "Trouble: LSP References" })
-    vim.keymap.set("n", "[d", function() require("trouble").next({ skip_groups = true, jump = true }) end, { desc = "Trouble: Next" })
+
+    local CamiloAvelar_Trouble = vim.api.nvim_create_augroup("CamiloAvelar_Trouble", {})
+    local autocmd = vim.api.nvim_create_autocmd
+    autocmd("BufWinEnter", {
+      group = CamiloAvelar_Trouble,
+      pattern = "*",
+      callback = function ()
+        if vim.bo.ft ~= "noice" then
+            return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        vim.keymap.set("n", "<leader>tn", function() require("trouble").next({ skip_groups = true, jump = true }) end, { buffer = bufnr, remap = false, desc = "Trouble: Next" })
+        vim.keymap.set("n", "<leader>tp", function() require("trouble").previous({ skip_groups = true, jump = true }) end, { buffer = bufnr, remap = false, desc = "Trouble: Previous" })
+      end,
+    })
   end,
 }

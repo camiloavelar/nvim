a82e375733afddcdeb399a95a92fc0a99977c180
Camilo Avelar <camilo.avelar@gerencianet.com.br>
2024-01-05 14:48:29 +0000

    feat(harpoon): add close menu with <C-c>

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -16,5 +16,13 @@ return {
     vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon file 3"})
     vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon file 4"})
     vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Harpoon file 5"})
+
+    harpoon:extend({
+      UI_CREATE = function(cx)
+        vim.keymap.set("n", "<C-c>", function()
+          harpoon.ui:close_menu()
+        end, { buffer = cx.bufnr })
+      end,
+    })
   end,
 }

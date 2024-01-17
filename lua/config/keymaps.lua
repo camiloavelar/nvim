c7cbeda6b2c302b9533e2922964d6f258547ac82
Camilo Avelar <camilo.avelar@gerencianet.com.br>
2024-01-17 16:11:09 +0000

    refactor(plugins): cleanup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,19 +7,15 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true, noremap = true })
 -- Buffer Navigation
 mapkey("<leader>bn", "bnext", "n") -- Next buffer
 mapkey("<leader>bp", "bprevious", "n") -- Prev buffer
-mapkey("<leader>bb", "e #", "n") -- Switch to Other Buffer
-mapkey("<leader>`", "e #", "n") -- Switch to Other Buffer
 
 -- Noice
 mapkey("<leader>fn", "Noice telescope", "n")
 mapkey("<leader>q", "Noice dismiss", "n")
 
 -- Directory Navigatio}n
-mapkey("<leader>m", "NvimTreeFocus", "n")
 mapkey("<leader>pv", ":Ex", "n")
 mapkey("<leader>e", "NvimTreeToggle", "n")
 
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
@@ -37,10 +33,6 @@ mapkey("<C-h>", "wincmd h", "t") -- Navigate Left
 mapkey("<C-j>", "wincmd j", "t") -- Navigate Down
 mapkey("<C-k>", "wincmd k", "t") -- Navigate Up
 mapkey("<C-l>", "wincmd l", "t") -- Navigate Right
-mapkey("<C-h>", "TmuxNavigateLeft", "n") -- Navigate Left
-mapkey("<C-j>", "TmuxNavigateDown", "n") -- Navigate Down
-mapkey("<C-k>", "TmuxNavigateUp", "n") -- Navigate Up
-mapkey("<C-l>", "TmuxNavigateRight", "n") -- Navigate Right
 
 -- Window Management
 mapkey("<leader>sv", "vsplit", "n") -- Split Vertically
@@ -58,13 +50,6 @@ vim.keymap.set("v", ">", ">gv", { silent = true, noremap = true })
 mapkey("<leader>pa", "echo expand('%:p')", "n") -- Show Full File Path
 
 local api = vim.api
-
--- Zen Mode
-api.nvim_set_keymap("n", "<leader>zn", ":TZNarrow<CR>", {})
-api.nvim_set_keymap("v", "<leader>zn", ":'<,'>TZNarrow<CR>", {})
-api.nvim_set_keymap("n", "<leader>sm", ":TZFocus<CR>", {})
-api.nvim_set_keymap("n", "<leader>zm", ":TZMinimalist<CR>", {})
-api.nvim_set_keymap("n", "<leader>za", ":TZAtaraxis<CR>", {})
 
 -- Comments
 api.nvim_set_keymap("n", "<C-_>", "<leader>ctl", { noremap = false })
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -21,7 +21,7 @@ return {
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-c>"] = cmp.mapping.abort(), -- close completion window 
+				-- ["q"] = cmp.mapping.abort(), -- close completion window 
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
 			-- sources for autocompletion

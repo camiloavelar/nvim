e6d163cd381030b9de1891ef98ec0c118bac6d5e
camiloavelar <camilo.avelar@avenue.us>
2026-09-14 10:48:58 -0300

    feat(markdown): enable obsidian.nvim and point at the Camilo vault

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -29,7 +29,7 @@ return {
 		version = "*",
 		event = "VeryLazy",
 		ft = "markdown",
-		enabled = false,
+		enabled = true,
 		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
 		-- event = {
 		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
@@ -49,7 +49,7 @@ return {
 				workspaces = {
 					{
 						name = "work",
-						path = "~/Documents/Obsidian/work",
+						path = "~/Documents/Obsidian/Camilo",
 					},
 				},
 				daily_notes = {
